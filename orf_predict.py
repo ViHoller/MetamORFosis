3cea0278ea4ea5b9b0e361ca55a5ca29c8613416
ViHoller <vinicius.hollermann@gmail.com>
2024-06-01 20:09:04 +0200

    Added docstrings to functions

--- orf_predict.py
+++ orf_predict.py
@@ -109,10 +109,18 @@ def remove_overlapping(orf_df, rev=False):
 
     return(orf_df)
 
-def merge_dfs(df1,df2): # marge dataframes and order them by position
+def merge_dfs(df1,df2):
+    '''
+    Merge orf dataframes into one and sort merged dataframe by orf starting position
+
+    Args:
+        df1 and df2 - orf dataframes from forward and reverse complement strands, order does not matter
+
+    Returns:
+        Merged and sorted dataframe with all identifed orfs
+    '''
     merged_df = pd.concat([df1,df2], axis=0)
     sorted_orfs = merged_df.sort_values(by='start_pos')
-    #print(df1.shape, df2.shape, merged_df.shape, sorted_orfs.shape)
     return sorted_orfs
 
 def get_orfs(genome, threshold):
@@ -140,6 +148,14 @@ def get_orfs(genome, threshold):
 
 
 def write_fasta(df, in_filename, out_filename):
+    '''
+    Write output fasta file in single-line fasta format
+
+    Arguments:
+        df - merged data and sorted dataframe of all orfs (from merge_dfs())
+        in_filename - name of input file for naming accessions
+        out_filename - name of file where orfs are written to
+    '''
     with open(out_filename, 'w') as out_f:
         for index, row in df.iterrows():
             out_f.write(f'>{in_filename}|{row['orf_id']}\n')
@@ -153,20 +169,20 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-    prog="",
-    description="",
+    prog="ORFan",
+    description=""
     )
     parser.add_argument(
     'input_filename',
-    help=""     
+    help="Name of fasta file containing the genome"     
     )
     parser.add_argument(
     'outfile',
-    help=""
+    help="Name of generate file containing all ORFs"
     )
     parser.add_argument(
     '--t',
-    help="threshold of ORF size", # better description
+    help="threshold of minimal ORF size",
     default=123 # ADD THE STANDARD THRESHOLD
     )
     main(parser.parse_args())
